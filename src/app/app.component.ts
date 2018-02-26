5d5628221860983acdf170ba58572a1cfb36f71e
Ted Bogin <ted.bogin@accenture.com>
2018-02-26 10:23:12 -0500

    Removed extra comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 
-
-//Base font size (16px) that can be changed
- //2 way binding?
-//1 input field
- //User can select whether they will input px or REM
-//Calculate button 
- //Press button to see the px or REM
-
-//Functions
- //Convert REM to px
- //Convert px to REM
- //handle click by calling REM or px convert functions depending on user selection
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -41,11 +28,9 @@ export class AppComponent {
 
   remToPx(rem: number, basePx = 26) {
     this.pxValue = (rem * basePx);
-    // return this.pxValue;
   }
 
   pixToREM(pixels : number, basePx = 16) {
     this.remValue = (pixels / basePx);
-    // return this.remValue;
   }
 }
